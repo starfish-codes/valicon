90ab3a5c59412410bcf9bed6ffbc79b2fac6c3c0
Diede <diede@starfish.team>
2024-07-15 20:06:56 +0200

    fix: mix.exs file

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule Valicon.MixProject do
   use Mix.Project
 
+  @name "Valicon"
+  @version "1.0.0"
+  @repo_url "https://github.com/starfish-codes/valicon"
+
   def project do
     [
       app: :valicon,
-      version: "1.0.0",
+      version: @version,
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
+      description:
+        "A comprehensive set of data structure validation and related helper functions, designed to work without external dependencies.",
+      package: package(),
       deps: deps(),
+      docs: docs(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
@@ -29,6 +37,21 @@ defmodule Valicon.MixProject do
       # For testing
       {:excoveralls, "~> 0.10", only: :test},
       {:faker, "~> 0.17", only: [:test]}
+    ]
+  end
+
+  defp package do
+    [
+      links: %{"GitHub" => @repo_url},
+      licenses: ["MIT"]
+    ]
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      source_url: @repo_url,
+      main: @name
     ]
   end
 end
